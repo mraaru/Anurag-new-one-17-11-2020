c27a793f462975141950d31100240631fc7d6e68
mraaru <70273817+mraaru@users.noreply.github.com>
2020-11-17 12:59:05 +0530

    all new one

---
+++ src/Component/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./App.css"
+import Home from "./Home"
+
+
+const App = () => {
+    return (
+        <>
+            
+<Home/>
+
+            </>
+    )
+}
+
+export default App;
---
+++ src/Component/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Top from "../../Pages/Component/Layout/Header/Common-header"
+import Footer from "../../Pages/Component/Layout/Footer"
+import Sidebar from "../../Pages/Component/Layout/Sidebar"
+const Home = () => {
+    return (
+        <>
+            <Top />
+            <Footer />
+            <Sidebar />
+            </>
+    )
+}
+export default Home;
+
+
